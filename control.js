f6b099caff295d2354b3f6a8003cf23084c405cb
Студенческая <@rbiu.local>
2025-03-11 16:53:43 +0500

    all

---
+++ control.js
@@ -0,0 +1,8 @@
+let rangePrice = document.querySelector('input[type="range"].filter-price__slider');
+console.log(rangePrice)
+rangePrice.style.setProperty('--value', rangePrice.value);
+rangePrice.style.setProperty('--min', rangePrice.min == '' ? '0' : rangePrice.min);
+rangePrice.style.setProperty('--max', rangePrice.max == '' ? '100' : rangePrice.max);
+rangePrice.addEventListener('input',
+    () => rangePrice.style.setProperty('--value', rangePrice.value)
+);
